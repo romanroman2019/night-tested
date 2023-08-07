d43a8f2c369808eb729e64dc938754628fe4ad73
romanroman2019 <romanroma@gmail.com>
2023-08-07 16:16:33 +0100

    zxs

--- Client/Program.cs
+++ Client/Program.cs
@@ -11,6 +11,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<BlogPostSummaryService>();
 builder.Services.AddScoped<BlogPostService>();
+builder.Services.AddStaticWebAppsAuthentication();
 
 await builder.Build().RunAsync();
 
